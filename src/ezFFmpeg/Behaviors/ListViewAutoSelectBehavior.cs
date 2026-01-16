f851e20bd03327c5b7383989e791dbfe55aa0302
ichiriki <ytaked46-dev@yahoo.co.jp>
2026-01-17 08:43:02 +0900

    修正:コメントを追加

--- src/ezFFmpeg/Behaviors/ListViewAutoSelectBehavior.cs
+++ src/ezFFmpeg/Behaviors/ListViewAutoSelectBehavior.cs
@@ -4,8 +4,25 @@ using System.Windows.Media;
 
 namespace ezFFmpeg.Behaviors
 {
+    /// <summary>
+    /// ListView 内のコントロールがフォーカスを取得した際に、
+    /// 自動的にその ListViewItem を選択状態にするための Attached Behavior。
+    ///
+    /// ・TextBox や Button などをクリックしたとき
+    /// ・フォーカス移動のみでも行選択を同期したい場合
+    ///
+    /// MVVM を崩さずに ListView の選択制御を補助します。
+    /// </summary>
     public static class ListViewAutoSelectBehavior
     {
+        // ------------------------------------------------------------------
+        // Attached Property 定義
+        // ------------------------------------------------------------------
+
+        /// <summary>
+        /// 自動選択機能を有効／無効にするための Attached Property。
+        /// true の場合、GotFocus イベントを監視します。
+        /// </summary>
         public static readonly DependencyProperty EnableAutoSelectProperty =
             DependencyProperty.RegisterAttached(
                 "EnableAutoSelect",
@@ -13,12 +30,26 @@ namespace ezFFmpeg.Behaviors
                 typeof(ListViewAutoSelectBehavior),
                 new PropertyMetadata(false, OnEnableAutoSelectChanged));
 
+        /// <summary>
+        /// EnableAutoSelect プロパティの setter。
+        /// </summary>
         public static void SetEnableAutoSelect(DependencyObject obj, bool value)
             => obj.SetValue(EnableAutoSelectProperty, value);
 
+        /// <summary>
+        /// EnableAutoSelect プロパティの getter。
+        /// </summary>
         public static bool GetEnableAutoSelect(DependencyObject obj)
             => (bool)obj.GetValue(EnableAutoSelectProperty);
 
+        // ------------------------------------------------------------------
+        // Attached Property 変更時の処理
+        // ------------------------------------------------------------------
+
+        /// <summary>
+        /// EnableAutoSelect の値が変更されたときに呼ばれます。
+        /// GotFocus イベントの購読／解除を行います。
+        /// </summary>
         private static void OnEnableAutoSelectChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
             if (d is not FrameworkElement fe)
@@ -30,6 +61,14 @@ namespace ezFFmpeg.Behaviors
                 fe.GotFocus += OnGotFocus;
         }
 
+        // ------------------------------------------------------------------
+        // フォーカス取得時の処理
+        // ------------------------------------------------------------------
+
+        /// <summary>
+        /// 対象要素がフォーカスを取得したときに呼ばれます。
+        /// 親の ListViewItem を特定し、対応する項目を選択状態にします。
+        /// </summary>
         private static void OnGotFocus(object sender, RoutedEventArgs e)
         {
             if (sender is not DependencyObject d)
@@ -44,6 +83,16 @@ namespace ezFFmpeg.Behaviors
             lv.SelectedItem = lvi.DataContext;
         }
 
+        // ------------------------------------------------------------------
+        // VisualTree ヘルパー
+        // ------------------------------------------------------------------
+
+        /// <summary>
+        /// VisualTree を親方向にたどり、指定した型の親要素を探します。
+        /// </summary>
+        /// <typeparam name="T">探索する親要素の型</typeparam>
+        /// <param name="child">探索開始点となる要素</param>
+        /// <returns>見つかった親要素。存在しない場合は null。</returns>
         private static T? FindParent<T>(DependencyObject child) where T : DependencyObject
         {
             DependencyObject parent = child;
--- src/ezFFmpeg/Behaviors/SliderTimeTooltipBehavior.cs
+++ src/ezFFmpeg/Behaviors/SliderTimeTooltipBehavior.cs
@@ -8,21 +8,56 @@ using System.Windows.Input;
 
 namespace ezFFmpeg.Behaviors
 {
+    /// <summary>
+    /// Slider 上にマウスを移動した際、
+    /// その位置に対応する再生時間をツールチップで表示し、
+    /// クリック時にはその時間へ即座にジャンプさせる Behavior。
+    ///
+    /// ・動画のシークバー用途を想定
+    /// ・MVVM を崩さずに UI 挙動を追加できる
+    /// </summary>
     public class SliderTimeTooltipBehavior : Behavior<Slider>
     {
+        // ------------------------------------------------------------------
+        // Attached Property 定義
+        // ------------------------------------------------------------------
+
+        /// <summary>
+        /// スライダー全体が表す総再生時間（秒）。
+        /// Slider の Value は「秒」を直接扱う想定。
+        /// </summary>
         public static readonly DependencyProperty TotalSecondsProperty =
             DependencyProperty.Register(nameof(TotalSeconds),
                 typeof(double),
                 typeof(SliderTimeTooltipBehavior));
 
+        /// <summary>
+        /// 総再生時間（秒）。
+        /// ViewModel からバインドされる想定。
+        /// </summary>
         public double TotalSeconds
         {
             get => (double)GetValue(TotalSecondsProperty);
             set => SetValue(TotalSecondsProperty, value);
         }
 
+        // ------------------------------------------------------------------
+        // フィールド
+        // ------------------------------------------------------------------
+
+        /// <summary>
+        /// マウス位置に応じた時間を表示するツールチップ。
+        /// </summary>
         private ToolTip? _toolTip;
 
+        // ------------------------------------------------------------------
+        // Behavior ライフサイクル
+        // ------------------------------------------------------------------
+
+        /// <summary>
+        /// Behavior が Slider にアタッチされたときに呼ばれます。
+        /// ツールチップの生成とマウスイベントの購読を行います。
+        /// </summary>
         protected override void OnAttached()
         {
             _toolTip = new ToolTip
@@ -38,6 +73,10 @@ namespace ezFFmpeg.Behaviors
             AssociatedObject.PreviewMouseLeftButtonDown += OnMouseLeftButtonDown;
         }
 
+        /// <summary>
+        /// Behavior がデタッチされたときに呼ばれます。
+        /// イベント購読を解除します。
+        /// </summary>
         protected override void OnDetaching()
         {
             AssociatedObject.MouseMove -= OnMouseMove;
@@ -45,6 +84,14 @@ namespace ezFFmpeg.Behaviors
             AssociatedObject.PreviewMouseLeftButtonDown -= OnMouseLeftButtonDown;
         }
 
+        // ------------------------------------------------------------------
+        // マウスイベント処理
+        // ------------------------------------------------------------------
+
+        /// <summary>
+        /// マウス移動時に呼ばれます。
+        /// マウス位置から再生時間を算出し、ツールチップに表示します。
+        /// </summary>
         private void OnMouseMove(object sender, MouseEventArgs e)
         {
             if (TotalSeconds <= 0) return;
@@ -62,12 +109,20 @@ namespace ezFFmpeg.Behaviors
             _toolTip.IsOpen = true;
         }
 
+        /// <summary>
+        /// マウスが Slider から離れたときに呼ばれます。
+        /// ツールチップを非表示にします。
+        /// </summary>
         private void OnMouseLeave(object sender, MouseEventArgs e)
         {
             if (_toolTip != null)
                 _toolTip.IsOpen = false;
         }
 
+        /// <summary>
+        /// マウス左クリック時に呼ばれます。
+        /// クリック位置に対応する時間へ Slider.Value を直接設定します。
+        /// </summary>
         private void OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
             if (TotalSeconds <= 0) return;
